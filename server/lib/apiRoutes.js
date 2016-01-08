7ee0f407fa684fca0dbfaa1630cde3fb13f34949
Matt Hand <mmhand123@gmail.com>
2016-01-07 20:04:03 -0500

    Fixed server crash issue

--- server/lib/apiRoutes.js
+++ server/lib/apiRoutes.js
@@ -2,8 +2,8 @@ var somethingWithDB;
 
 module.exports = function (app) {
 
-  app.param(['type', 'prop', 'query'], )
+  // app.param(['type', 'prop', 'query']);
 
-  app.get('/:type/:prop/:query', )
+  // app.get('/:type/:prop/:query');
 
 };
