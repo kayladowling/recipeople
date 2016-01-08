f2cc65e7afd1df5e0252b35c3adfafdf655c9c3a
delventhalz <delventhalz@gmail.com>
2016-01-08 16:28:36 -0600

    (feat) Built basic dummy recipe to display.

--- public/recipe/recipe.js
+++ public/recipe/recipe.js
@@ -1,5 +1,19 @@
 angular.module('Recipeoples.recipe', [])
 
-.controller('RecipeController', function($scope){
-  $scope.hello = "recipe template";
+.controller('RecipeController', function($scope, $rootScope){
+  //TODO: Delete this preseeded focus recipe when being properly set elsewhere
+  $rootScope.focusRecipe = {
+    title: 'Awesome Burger',
+    image_url: 'http://dxz0iw137d3y6.cloudfront.net/wp-content/uploads/2014/05/BrantLakeWagyu.jpg',
+    ingredients: ['One Bun', 'One Burger', 'Like a ton of Cheese'],
+    directions: 'Put burger in bun. Spread cheese. Heartattack.',
+    author: 'hjgfskjhfglsj',
+    likedBy: [],
+    dislikedBy: [],
+    groups: [],
+    reviews: []
+  };
+
+  var recipe = $scope.recipe = $rootScope.focusRecipe;
+  
 });
