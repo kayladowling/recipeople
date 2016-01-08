c5937232f3966f23e4dffce07a1248531bf04fac
Matt Hand <mmhand123@gmail.com>
2016-01-07 21:07:11 -0500

    Updated get route to pull test recipe from db and send

--- server/lib/apiRoutes.js
+++ server/lib/apiRoutes.js
@@ -1,18 +1,24 @@
 var util = require('./util.js');
+var Recipe = require('../db/models/recipe.js');
 var somethingWithDB = function(req, res) {
 
   // mock data, real data would come from db
-  var data = [{
-    id: req.params.id,
-    title: 'A Test Recipe',
-    ingredients: ['1/2 apple', '1/2 orange', '2 cups spinach'],
-    author: { id: 1, username: 'Matt'}
-  }];
-  res.body = data;
-  console.log('type: ', req.params.type);
+  // var data = [{
+  //   id: req.params.id,
+  //   title: 'A Test Recipe',
+  //   ingredients: ['1/2 apple', '1/2 orange', '2 cups spinach'],
+  //   author: { id: 1, username: 'Matt'}
+  // }];
+  // res.body = data;
+  Recipe.findOne({ 'title': 'Fried Pickles'}, function (err, recipe) {
+    console.log(recipe);
+    res.body = [recipe];
+    util.sendResponse(req, res);
+  });
+  /*console.log('type: ', req.params.type);
   console.log('prop: ', req.params.prop);
   console.log('query: ', req.params.query);
-  util.sendResponse(req, res);
+  util.sendResponse(req, res);*/
 };
 
 module.exports = function (app) {
