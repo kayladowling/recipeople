f6ca96cfa187e54557dc0b927421725bfc105b4e
kayladowling <kaydowling2@gmail.com>
2016-01-08 14:14:25 -0600

    almost completed post

--- public/post/post.js
+++ public/post/post.js
@@ -1,12 +1,33 @@
 angular.module('Recipeople.post', [])
 
-.controller('PostController', function($scope, $location){
+.controller('PostController', function($scope, $location, PostRecipeFactory){
  $scope.recipe = {};
 
- $scope.post = function() {
-  Recipeople.post($scope.recipe)
-    .then() {
-      $
-    }
- }
+ $scope.postRecipe = function() {
+  PostRecipeFactory.post($scope.recipe);
+ };
 })
+
+.factory('PostRecipeFactory', function($http) {
+  var postRecipe = function() {
+    return $http({
+      method: 'POST'
+      url: '/post'
+      data: {
+        recipes : title,
+        recipe : image_url,
+        recipe: ingredients, 
+        recipe: directions,
+        recipe: author,
+        recipe: likedBy,
+        recipe: dislikedBy,
+        recipe: groups,
+        recipe: reviews,
+      }
+    })
+    .then(function(response) {
+      return response.data;
+    });
+  }
+
+});
