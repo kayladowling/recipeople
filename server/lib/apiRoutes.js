c36517f8dbb4fd08b475ccf71baeec27bcbe6af4
Matt Hand <mmhand123@gmail.com>
2016-01-08 13:12:27 -0500

    Work on building out db interaction

--- server/lib/apiRoutes.js
+++ server/lib/apiRoutes.js
@@ -13,7 +13,7 @@ var somethingWithDB = function(req, res) {
   Recipe.find({ 'title': 'Fried Pickles'}, function (err, recipe) {
     console.log(recipe);
     res.body = [recipe];
-    util.sendResponse(req, res);
+    util.sendResponse(req, res, 200);
   });
   /*console.log('type: ', req.params.type);
   console.log('prop: ', req.params.prop);
@@ -24,15 +24,20 @@ var somethingWithDB = function(req, res) {
 var somethingElseWithDB = function (req, res) {
   console.log(req.params.action); // createRecipe
   console.log(req.body); // {}
-  util.sendResponse(req, res);
+  util.sendResponse(req, res, 201);
 };
 
 module.exports = function (app) {
 
-  // everything in here is /api/
+  // everything in here is from the /api endpoint
 
+  // get requests come in the /type/prop/query format
+  // e.g., /recipes/title/Fried Pickles
   app.get('/:type/:prop/:query', somethingWithDB);
 
+
+  // post requests come in the /action format with the additions in the body in json
+  // e.g., /createRecipe, /likeRecipe, etc.
   app.post('/:action', somethingElseWithDB);
 
 };
