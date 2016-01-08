b2373f97fc0a1cce3accbb80564421a1ca9bc4b7
Matt Hand <mmhand123@gmail.com>
2016-01-07 19:22:10 -0500

    Worked on routing get api requests

---
+++ server/lib/apiRoutes.js
@@ -0,0 +1,9 @@
+var somethingWithDB;
+
+module.exports = function (app) {
+
+  app.param(['type', 'prop', 'query'], )
+
+  app.get('/:type/:prop/:query', )
+
+};
--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
 
 app.use('/', express.static(__dirname + '/../public'));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
   res.render('index');
 });
+
+
+// grab all api requests and send them to the
+// api router
+var apiRoutes = express.Router();
+app.get('/api', apiRoutes);
+require('./lib/apiRoutes.js')(apiRoutes);
 
 module.exports = app;
