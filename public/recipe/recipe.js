24ee08db608babc0c9ec1e30e31b4b8b32c56c76
delventhalz <delventhalz@gmail.com>
2016-01-08 18:04:22 -0600

    (feat) Build out basic recipe template with dummy data.

--- public/recipe/recipe.js
+++ public/recipe/recipe.js
@@ -1,13 +1,14 @@
 angular.module('Recipeoples.recipe', [])
 
-.controller('RecipeController', function($scope, $rootScope){
-  //TODO: Delete this preseeded focus recipe when being properly set elsewhere
-  $rootScope.focusRecipe = {
+
+.controller('RecipeController', function($scope, $rootScope, RecipeFactory){
+  // TODO: Delete this preseeded focus recipe once being set elsewhere
+  $rootScope.focusRecipe = $rootScope.focusRecipe || {
     title: 'Awesome Burger',
     image_url: 'http://dxz0iw137d3y6.cloudfront.net/wp-content/uploads/2014/05/BrantLakeWagyu.jpg',
     ingredients: ['One Bun', 'One Burger', 'Like a ton of Cheese'],
-    directions: 'Put burger in bun. Spread cheese. Heartattack.',
-    author: 'hjgfskjhfglsj',
+    directions: 'Put burger in bun. Spread cheese. Have a heartattack.',
+    author: 1,
     likedBy: [],
     dislikedBy: [],
     groups: [],
@@ -15,5 +16,32 @@ angular.module('Recipeoples.recipe', [])
   };
 
   var recipe = $scope.recipe = $rootScope.focusRecipe;
-  
+
+  // TODO: Refactor below to work with promise.
+  var authorName = $scope.authorName = RecipeFactory.getAuthorName(recipe.author);
+
+})
+
+
+.factory('RecipeFactory', function($http){
+
+  var getAuthorName = function (userid){
+    return 'Awesome Dude';
+
+  //  TODO: Comment in once users db query is in place.
+  //  return $http({
+  //   method: 'GET',
+  //   url: "/api/users/id/" + userid
+  //  })
+  //   .then(function(res){
+  //     console.log('Got recipe author', res.data);
+
+  //     return res.data[0].username;
+  //   });
+  };
+
+
+  return {
+    getAuthorName: getAuthorName
+  };
 });
