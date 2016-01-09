5f3ebebdaba56f84d32eff2586ab87ff1bcc80ca
kayladowling <kaydowling2@gmail.com>
2016-01-08 18:33:21 -0600

    in process on templating landing page

--- public/landing/landing.js
+++ public/landing/landing.js
@@ -1,5 +1,27 @@
 angular.module('Recipeoples.landing', [])
 
-.controller('LandingController', function($scope){
-  $scope.hello = "You have landed."
+.controller('LandingController', function($scope, $location, LandingFactory){
+  $scope.recipe = {};
+
+  LandingFactory.getRecipe(1).then(function(recipe) {
+    $scope.recipe = recipe;
+  });
 })
+
+.factory('LandingFactory', function($http) {
+
+  var getRecipe = function(userid) {
+    return $http({
+      method: 'GET',
+      url:'/api/recipes/author/' + userid
+    })
+    .then(function(resp) {
+      return resp.data[0];
+    });
+  }
+
+  return {
+  	getRecipe: getRecipe
+  }
+
+ }); 
