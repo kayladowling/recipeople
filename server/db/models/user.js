216c4f8de40da7d6aa6749ea18e6c5d92ccdb2c5
Matt Hand <mmhand123@gmail.com>
2016-01-10 15:00:39 -0500

    (feat) Added password field, password checking, and password hashing to user schema

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,8 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
+var Promise = require('bluebird');
+bcrypt = Promise.promisifyAll(bcrypt);
+
 
 var UserSchema = new Schema({
   username: String,
+  password: String,
   image_url: String,
   liked: Array,
   disliked: Array,
@@ -10,6 +15,27 @@ var UserSchema = new Schema({
   authored: Array,
   testid: Number,
   friends: Array
+});
+
+
+UserSchema.methods.checkPassword = function (enteredPassword) {
+  return bcrypt.compareAsync(enteredPassword, this.password);
+};
+
+UserSchema.pre('save', function (next) {
+
+  var user = this;
+
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.saltAsync(10).then(function(salt) {
+    bcrypt.hashAsync(user.password, salt, null).then(function(hash) {
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 module.exports = mongoose.model('User', UserSchema);
