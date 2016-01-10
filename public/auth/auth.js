51424dcae9f8b333f8d01d4dbe2d72a112f466a6
delventhalz <delventhalz@gmail.com>
2016-01-10 10:14:07 -0600

    (feat) Fill out auth.js with necessary controller and factory methods.

--- public/auth/auth.js
+++ public/auth/auth.js
@@ -1,5 +1,54 @@
 angular.module('Recipeoples.auth', [])
 
-.controller('AuthController', function($scope){
-  $scope.hello = "auth template";
+
+.controller('AuthController', function ($scope, $rootScope, $location, $window, AuthFactory) {
+  $rootScope.currentUser = null;
+
+  // Passes in the user to be authenticated, either as new or returning.
+  $scope.submit = function() {
+    AuthFactory.authenticate($scope.user, $scope.signup)
+    .then(function (token) {
+      $window.localStorage.setItem('com.recipeople', token);
+      $rootScope.currentUser = token.user;
+      $location.path('/');
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  };
+
+})
+
+
+.factory('AuthFactory', function ($http, $location, $window) {
+
+  //Sends an authentication query either as a new user, or returning.
+  var authenticate = function(user, signup) {
+    var path = signup ? 'signup' : 'signin';
+
+    return $http({
+      method: 'POST',
+      url: '/api/users/' + path,
+      data: user
+    })
+    .then(function(resp) {
+      return resp.data.token;
+    });
+  };
+
+  var isAuth = function() {
+    return !!$window.localStorage.getItem('com.recipeople');
+  };
+
+  var signout = function() {
+    $window.localStorage.removeItem('com.recipeople');
+    $location.path('/signin');
+  };
+
+  return {
+    authenticate: authenticate,
+    isAuth: isAuth,
+    signout: signout
+  };
+
 });
