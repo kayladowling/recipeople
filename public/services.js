88e4e4d93fb68ef1bd78fa7179e92d7649682de4
delventhalz <delventhalz@gmail.com>
2016-01-11 14:31:08 -0600

    (fix) Cleaned up services, and properly exported Post methods

--- public/services.js
+++ public/services.js
@@ -3,7 +3,7 @@ angular.module('Recipeoples.services', [])
 
 .factory('getFactory', function($http){
 
-  // TODO: Delete these dummy objects once all DB routes are functioning.
+  // TODO: Delete this dummy object once all DB routes are functioning.
   var dummyUser = {
     username: 'Bob',
     image_url: 'https://pbs.twimg.com/profile_images/1044973752/390dfbe9-eccf-41f9-822e-17c8d4c251b4.jpg',
@@ -13,17 +13,6 @@ angular.module('Recipeoples.services', [])
     authored: [],
     testid: 1,
     friends: []
-  };
-  var dummyRecipe = {
-    title: 'Awesome Burger',
-    image_url: 'http://dxz0iw137d3y6.cloudfront.net/wp-content/uploads/2014/05/BrantLakeWagyu.jpg',
-    ingredients: ['One Bun', 'One Burger', 'Like a ton of Cheese'],
-    directions: 'Put burger in bun. Spread cheese. Have a heartattack.',
-    author: 1,
-    likedBy: ['Bob', 'Bud', 'Boshika'],
-    dislikedBy: [],
-    groups: ['Burgers Anonymous', 'Moose Lodge', 'HiRs'],
-    reviews: []
   };
 
 
@@ -252,4 +241,10 @@ angular.module('Recipeoples.services', [])
     });
   };
 
+  return {
+    newRecipe: newRecipe,
+    newGroup: newGroup,
+    joinGroup: joinGroup,
+    recipeToGroup: recipeToGroup
+  };
 });
