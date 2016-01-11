74269815119a215efa626f59d889a60d43488391
Matt Hand <mmhand123@gmail.com>
2016-01-11 16:21:40 -0500

    (feat) Added route handling for updating recipes and groups and basic update functions

--- server/lib/controllers/userController.js
+++ server/lib/controllers/userController.js
@@ -1,5 +1,4 @@
 var User = require('../../db/models/user.js');
-var jwt = require('jwt-simple');
 var util = require('../util.js');
 
 module.exports = {
@@ -14,21 +13,8 @@ module.exports = {
         } else {
           user.checkPassword(password, function(isMatch) {
             if (isMatch) {
-              var token = jwt.encode(user, 'nyannyannyan');
-              res.json(
-                {
-                  token: token,
-                  user: {
-                    username: user.username,
-                    image_url: user.image_url,
-                    liked: user.liked,
-                    disliked: user.disliked,
-                    groups: user.groups,
-                    authored: user.authored,
-                    friends: user.friends
-                  }
-                }
-              );
+              var token = util.encodeToken(user);
+              res.json({token: token});
             } else {
               return next(new Error('Password did not match'));
             }
@@ -56,27 +42,20 @@ module.exports = {
           newUser.password = password;
           User.create(newUser, function(createdUser) {
             console.log('created');
-              var token = jwt.encode(user, 'nyannyannyan');
+              var token = util.encode(user);
               res.json({token: token});
             });
         }
     });
   },
-  checkToken: function (req, res, next) {
+  currentUser: function (req, res, next) {
     var token = req.headers['x-access-token'];
     if (!token) {
       next(new Error('No access token!'));
     } else {
-      var user = jwt.decode(token, 'nyannyannyan');
-      User.findOne({username: user.username}, 'username image_url liked disliked groups authored friends').exec()
-        .then(function(user) {
-          if (!user) {
-            next(new Error('Invalid token'));
-          } else {
-            res.body = user;
-            util.sendResponse(req, res, 200);
-          }
-        });
-    }
+      var user = util.decode(req);
+        res.body = user;
+        util.sendResponse(req, res, 200);
+      }
   }
 }
